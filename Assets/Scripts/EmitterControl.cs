6ccf834f15a35288295ea680c59337fb29871896
Hiromi <hiromi.cota@gmail.com>
2018-02-13 18:36:43 -0800

    Expanded level & controls
    Camera controls now work OK. Gonna tweak them a bit.

---
+++ Assets/Scripts/EmitterControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterControl : MonoBehaviour {
+    public Rigidbody ball;
+    public ParticleSystem sparks;    
+
+	// Use this for initialization
+	void Start () {      
+        sparks.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ball.IsSleeping())
+            sparks.Stop();
+        else
+            sparks.Play();
+	}
+}
