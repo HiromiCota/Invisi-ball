895c97b75eab935e9788fb027688598d9d6c84d2
Hiromi <hiromi.cota@gmail.com>
2018-02-28 13:29:51 -0800

    Emitter tied to velocity
    Also detects collisions

--- Assets/Scripts/EmitterControl.cs
+++ Assets/Scripts/EmitterControl.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class EmitterControl : MonoBehaviour {
     private Rigidbody ball;
-    public ParticleSystem sparks;    
+    public ParticleSystem sparks;
+    [SerializeField] float particleMultiplier;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         ball = GetComponent<Rigidbody>();
         //sparks.Stop();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (ball.IsSleeping()) {
+
+    // Update is called once per frame
+    void Update() {
+        var main = sparks.main;
+        main.maxParticles = (int)((Vector3.Distance(ball.velocity, Vector3.zero)) * particleMultiplier);
+        if (main.maxParticles < 10) {
             sparks.Pause();
             sparks.Clear();
         } else
             sparks.Play();
-	}
+    }
 }
---
+++ Assets/Scripts/VictoryCondition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryCondition : MonoBehaviour {
+    int Level; // What level are we on?
+    Rigidbody ball;
+    bool readyForWin;
+    [SerializeField] Text victoryText;
+    
+    // Use this for initialization
+    void Start() {
+        ball = GetComponent<Rigidbody>();
+        Level = 1;
+        readyForWin = true;
+        victoryText.enabled = false;
+    }
+           
+    void OnCollisionEnter(Collision col) {
+        //Debug.Log("Collision detected.");
+        if (readyForWin && col.collider.tag == "floor") {
+            Victory(Level++);
+            readyForWin = false;
+        }
+        if (col.collider.tag == "block") {
+            BlockHit(col);
+        }
+    }
+
+    void Victory(int level) {
+        string victory = "You've completed Level " + level + "!";
+        victoryText.text = victory;
+        victoryText.enabled = true;
+    }
+
+    void BlockHit(Collision col) {
+        // Trigger the block's emitter. 
+        Debug.Log(col.gameObject.name + " hit.");        
+    }
+}
